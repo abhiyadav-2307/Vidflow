5a7f7d6cc059f7a8a63659af472655558cd130e7
Abhishek Yadav <abhi1611919@gmail.com>
2024-10-15 10:51:59 +0530

    All setup configured some bugs removed

--- src/app.js
+++ src/app.js
@@ -26,4 +26,13 @@ app.use(
 app.use(express.static("public"));
 app.use(cookieParser())
 
+
+//routes import
+import userRouter from "./routes/user.routes.js"
+
+
+//routes declaration
+app.use("/api/v1/users",userRouter)
+//link will be-> http://localhost:8000/api/v1/user/register
+
 export { app };
